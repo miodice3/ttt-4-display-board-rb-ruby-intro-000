71e04bfe37e4476ae9f1edb40e3bda9973454088
Michael I <miodice3@gmail.com>
2020-08-13 21:46:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,11 +5,11 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
   def display_board(argument)
-    puts " #{argument[0]} | #{argument[1]} | #{argument[2]}"
+    puts " #{argument[0]} | #{argument[1]} | #{argument[2]} "
     puts "-----------"
-    puts " #{argument[3]} | #{argument[4]} | #{argument[5]}"
+    puts " #{argument[3]} | #{argument[4]} | #{argument[5]} "
     puts "-----------"
-    puts " #{argument[6]} | #{argument[7]} | #{argument[8]}"
+    puts " #{argument[6]} | #{argument[7]} | #{argument[8]} "
   end
 
 display_board(board)
