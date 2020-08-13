223165dd3c54befede4d115ec271293adbd0787c
Michael I <miodice3@gmail.com>
2020-08-13 21:40:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = ["1","2","3","4","5","6","7","8","9"]
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 
   def display_board(argument)
     puts " #{argument[0]} | #{argument[1]} | #{argument[2]}"
